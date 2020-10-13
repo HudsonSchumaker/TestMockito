d76e1d0b71e1c1a4fba1ee48c4a46bd2b571a3f6
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2020-10-13 11:20:13 +0200

    Create CompanyClientImpl.java

---
+++ src/main/java/br/com/schumaker/invocation/CompanyClientImpl.java
@@ -0,0 +1,16 @@
+package br.com.schumaker.invocation;
+
+/**
+ *
+ * @author hudson.schumaker
+ */
+ 
+public class CompanyClientImpl {
+
+   private IdCryptoTool idCryptoTool;
+   
+   public String getCompanyName(String encripted) {
+     // Just for test
+    return "eee";
+   }
+}
